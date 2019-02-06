93b88fc6a6c1f93b09b41138961f2ed06744cb41
Oki Fatahillah <okifatahillah@gmail.com>
2019-02-06 16:57:11 +0700

    lacar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div>
 <<<<<<< HEAD
-	    <h1>hai sayang, Love You :*</h1>
+	    <h1>hai sayang, Love You :* <3 </h1>
 =======
 	   <h1> Hai sayangku, Love You :*</h1>
 >>>>>>> ca9661b3c10688b08a74fdf6c3f0483eaa5d4244
