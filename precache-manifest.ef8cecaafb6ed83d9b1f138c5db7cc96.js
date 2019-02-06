f7a81d70de7a723f8623d254c5615cd0e2bb2086
Oki Fatahillah <okifatahillah@gmail.com>
2019-02-06 15:33:46 +0700

    Updates

---
+++ precache-manifest.ef8cecaafb6ed83d9b1f138c5db7cc96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "d490f72d1d326f092de9",
+    "url": "/hello/static/js/main.d490f72d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/hello/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d490f72d1d326f092de9",
+    "url": "/hello/static/css/main.ad1153c5.chunk.css"
+  },
+  {
+    "revision": "7d70ebc0a7aa5790ded3373a5a2f81c0",
+    "url": "/hello/index.html"
+  }
+];
