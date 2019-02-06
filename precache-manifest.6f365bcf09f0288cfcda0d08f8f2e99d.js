5d190ac619798f4dba2e579645bb4b44b906940e
Oki Fatahillah <okifatahillah@gmail.com>
2019-02-06 17:10:44 +0700

    Updates

---
+++ precache-manifest.6f365bcf09f0288cfcda0d08f8f2e99d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d748546a5aa69a5ab5f",
+    "url": "/static/js/main.0d748546.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "0d748546a5aa69a5ab5f",
+    "url": "/static/css/main.e9ef5260.chunk.css"
+  },
+  {
+    "revision": "f5746fed74a11bb8f07cca383a8f8fcd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hello/precache-manifest.71bb92d9f637d3575d9a746d5c2dbefc.js"
+  "/precache-manifest.6f365bcf09f0288cfcda0d08f8f2e99d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hello/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
